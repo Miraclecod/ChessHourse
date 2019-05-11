4b201ea9d2b9b796a7fa0c74916a4d590b5d86b0
Miraclecod <ser,mikelanjelo@yandex.ru>
2019-05-12 00:16:12 +0300

    next

--- script.js
+++ script.js
@@ -3,17 +3,21 @@
 new Vue({
     el: '#app',
     data: {
-        location: null,
-        possiblePositions: ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'B1', 'B2', 'B3', 'B4', 'B5',
-                            'B6', 'B7', 'B8', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'D1', 'D2', 'D3',
-                            'D4', 'D5', 'D6', 'D7', 'D8', 'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8',
-                            'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6',
-                            'G7', 'G8', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8'
-    ]
+        location: null
     },
-    computed: {
+    methods: {
         possibleMoves() {
-            
+            let x = +this.location[0];
+            let y = +this.location[1];
+            let all = [];
+            for(let i = 1; i < 9; i++){
+                for(let j = 1; j < 9; j++){
+                    if(((Math.abs(i - y)== 1) && (Math.abs(j - x) == 2)) || ((Math.abs(i - y)== 2) && (Math.abs(j - x) == 1))){
+                        all.push( j + '' + i);
+                    }
+                }
+            }
+            alert('Возможные варианты хода: ' + all);
         }
     }
 });
